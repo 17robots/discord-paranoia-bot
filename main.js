d3646c56478913139c3c0ab409df9a1d50c5359a
17robots <mdray@ameritech.net>
2018-03-05 23:16:59 -0500

    got random player order to work

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var help = require("./help.js");
 var personOrder = [];
 var inProgress = false;
 
-
+function hasElement(array, element) {
+    return array.indexOf(element) != -1;
+}
 
 function parsemsg(msg) {
     if (msg.content.toLowerCase().substr(0,1) !== "!") {
@@ -38,17 +40,45 @@ function parsemsg(msg) {
             } else {
                 inProgress = true;
 
+                // make the array of players first 
+                var players = client.users.array();
                 //create the player order
-                for(var i = 0; i < client.users.length; i++) {
+                while(personOrder.length < players.length) {
+                    var userIndex = Math.floor(Math.random() * players.length);
 
+                    if (hasElement(personOrder, userIndex)) {
+                        continue;
+                    } else {
+                        personOrder.push(userIndex);
+                    }
                 }
+
+                // replace indexes with players in that index
+                for(var i = 0; i < players.length; i++) {
+                    personOrder[i] = players[personOrder[i]];
+                }
+
+                var index;
+
+                // remove the botplayer
+                for (p in personOrder) {
+                    if (p.username === "paranoia-bot") {
+                        index = bot;
+                    }
+                }
+
+                personOrder.filter(function(i) {
+                    return i.username != "paranoia-bot";
+                });
+
+                msgObj.reply('Created player order');
             }
             break;
         case "order":
             // msgObj.reply("order does not work just yet please hold on");
             if(inProgress) {
                 var returnString = `the player order is as follows: ` + personOrder[0].username;
-                for (var i = 0; i < personOrder.length; i++) {
+                for (var i = 1; i < personOrder.length; i++) {
                     returnString += (' -> ' + personOrder[i].username);
                 }
                 msgObj.reply(returnString);
@@ -62,6 +92,10 @@ function parsemsg(msg) {
         case "specify":
             msgObj.reply("specify does not work just yet please hold on");
             break; 
+        case "end":
+            inProgress = false;
+            msgObj.reply('FOR TESTING ONLY PLEASE DELETE THIS LATER, but the game has ended.');
+            personOrder = [];
     }
 }
 //  i like cats?
